b98cea410dcb41ec9e29063f8320ee772a13067c
arijitdeb1502 <arijitdeb.git@gmail.com>
2019-10-26 12:06:17 +0800

    Installing typeface-roboto in the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './index.css';
+import './common/common.css';
+import 'typeface-roboto';
 import Controller from './screens/Controller';
 import * as serviceWorker from './serviceWorker';
 
