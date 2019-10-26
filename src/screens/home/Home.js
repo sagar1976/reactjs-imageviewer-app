5f045bd24aa7e59f91e8fcc3768eb04efda48696
arijitdeb1502 <arijitdeb.git@gmail.com>
2019-10-26 11:59:59 +0800

    Creating the initial project structure

---
+++ src/screens/home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.css';
+
+export default ()=>(
+    <div>
+        <h1>This is Home page</h1>
+    </div>
+)
---
+++ src/screens/login/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Login.css';
+
+export default ()=>(
+    <div>
+        <h1>This is login page</h1>
+    </div>
+)
