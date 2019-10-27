5fc47ed5ae6b597912d22661a1237a3d023c5903
Auritra Deb <45408887+auritra88@users.noreply.github.com>
2019-10-27 12:58:24 +0800

    Completed the Home page

---
+++ src/common/utils.js
@@ -0,0 +1,5 @@
+export const constants = {
+  userInfoUrl:"https://api.instagram.com/v1/users/self",
+  userMediaUrl:"https://api.instagram.com/v1/users/self/media/recent",
+  accessToken:"8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784"
+}
