47e8896ea2b7fd7438f5bc370296e4b3cac9cd75
sagar1976 <narvekar.sagar@gmail.com>
2019-10-28 18:32:37 +0530

    formating for header.js and index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './screens/login/Login';
 
-
 ReactDOM.render(<Login />, document.getElementById('root'));//Intial landing page as Login page is coded here
