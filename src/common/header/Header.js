4c322b186413b9390d0d32cd4c518e961abcd90d
sagar1976 <narvekar.sagar@gmail.com>
2019-10-28 17:28:34 +0530

    added Header.js and Header.css

---
+++ src/common/header/Header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+
+    render(){
+        return(
+            <div>
+            <header className="app-header">
+                <div className="app-logo">
+                    <span>Image Viewer</span>
+                </div>
+            </header>
+            </div>
+        )
+    }
+}
+export default Header;
