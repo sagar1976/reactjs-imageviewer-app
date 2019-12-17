9f43a1b470838eb6416ccc5cbcf2f869beffadb9
sagar1976 <narvekar.sagar@gmail.com>
2019-12-17 08:13:34 +0530

    added comments for routing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './screens/home/Home';
 import Login from './screens/login/Login';
 import Profile from './screens/profile/Profile';
 
-const App = () => (
+const App = () => (//routing between different pages like login, home, profile
   <Switch>
     <Route exact path='/' render={({history}, props) => <Login {...props} history={history}/>} />
     <Route exact path='/home' render={({history}, props) => <Home {...props} history={history}/>} />
